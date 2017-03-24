b9f4680b6b3a1b83df50ecef0759dad75023615d
Jason Brown <jasontbrown99@gmail.com>
2017-03-24 10:36:54 -0700

    add edit action to question-tile.js

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
+    edit(question, params) {
+      this.sendAction('edit', question, params);
+    },
     delete(question) {
       if (confirm('Are you sure you want to delete this question?')) {
         this.sendAction('destroyQuestion', question);
