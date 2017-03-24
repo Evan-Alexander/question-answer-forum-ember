a75cba46ee0bd819e5dde5ba0dded2411abb8aac
Jason Brown <jasontbrown99@gmail.com>
2017-03-24 10:12:27 -0700

    add saveQuestion action

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -5,6 +5,18 @@ export default Ember.Component.extend({
   actions: {
     questionFormShow() {
       this.set('addNewQuestion', true);
+    },
+
+    saveQuestion() {
+      var params = {
+        author: this.get('author'),
+        title: this.get('title'),
+        content: this.get('content'),
+        notes: this.get('notes'),
+        image: this.get('image'),
+      };
+      this.set('addNewQuestion', false);
+      this.sendAction('saveQuestion', params);
     }
   }
 });
