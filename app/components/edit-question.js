ed98dde51e724d3242698fcca06aad71bcdea28b
Jason Brown <jasontbrown99@gmail.com>
2017-03-24 10:34:06 -0700

    add editQuestionForm and edit actions

--- app/components/edit-question.js
+++ app/components/edit-question.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  editQuestionForm: false,
+  actions: {
+    editQuestionForm() {
+      this.set('editQuestionForm', true);
+    },
+    edit(question) {
+      var params {
+        author: this.get('author'),
+        title: this.get('title'),
+        content: this.get('content'),
+        notes: this.get('notes'),
+        image: this.get('image'),
+      };
+      this.set('editQuestionForm', false);
+      this.sendAction('edit', question, params);
+    }
+  }
 });
