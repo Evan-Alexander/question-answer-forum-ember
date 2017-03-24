76fae7186a8054b7484be25d08a2aba8e9c2cce9
Jason Brown <jasontbrown99@gmail.com>
2017-03-24 10:06:25 -0700

    add action to show form

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addNewQuestion', true);
+    }
+  }
 });
