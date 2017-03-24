6ec444b3284ecb100b8ed336583727d7b1909b0b
Jason Brown <jasontbrown99@gmail.com>
2017-03-24 13:16:49 -0700

    add formshow and save actions

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewAnswer: false,
+  actions: {
+    answerFormShow() {
+      this.set('addNewAnswer', true);
+    },
+    saveAnswer() {
+      var params = {
+        author: this.get('author'),
+        content: this.get('content')
+      };
+      this.get('addNewAnswer', false);
+      this.sendAction('saveAnswer', params);
+    }
+  }
 });
