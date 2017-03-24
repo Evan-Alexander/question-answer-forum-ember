6475fc2cc43349c56e8ca66bfb84f3cd84d4b9fd
Jason Brown <jasontbrown99@gmail.com>
2017-03-24 13:28:42 -0700

    add hasMany keyword to answers; also forgot to add image in attributes

--- app/models/question.js
+++ app/models/question.js
@@ -4,5 +4,7 @@ export default DS.Model.extend({
   author: DS.attr(),
   title: DS.attr(),
   content: DS.attr(),
-  notes: DS.attr()
+  notes: DS.attr(),
+  image: DS.attr(),
+  answers: DS.hasMany('answer', { async: true})
 });
